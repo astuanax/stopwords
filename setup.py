a3cccfd235b59ed03bbb55020ad861908ee7bd94
Astuanax <len@astuanax.com>
2015-11-23 11:21:58 +0100

    Bump.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='stopwords',
-    version='0.1.1',
+    version='0.1.2',
     description="Stopwords filter for 40 languages",
     long_description=readme + '\n\n' + history,
     author="Len Dierickx",
--- stopwords/__init__.py
+++ stopwords/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'Len Dierickx'
 __email__ = 'len@astuanax.com'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 
 import os
