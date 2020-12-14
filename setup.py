73c906acc8871c8d5e2103eb23f238cf7974f03b
astuanax <len.dierickx@gmail.com>
2020-12-14 11:53:49 +0100

    Meta updates

--- setup.py
+++ setup.py
@@ -48,13 +48,15 @@ setup(
     zip_safe=False,
     keywords=['stopwords','language processing','nlp','filter'],
     classifiers=[
-        'Development Status :: 4 - Beta',
+        '5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: ISC License (ISCL)',
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence'
     ],
     #test_suite='tests',
     #tests_require=test_requirements
